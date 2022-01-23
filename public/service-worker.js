a6afd42cf5aa73c0a81965b8f22faf54dacb2124
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-22 21:20:36 -0500

    code clean up

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,19 @@
 const CACHE_NAME = 'budget-tracker-v1';
 const DATA_CACHE_NAME = 'budget-cache-v1';
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/manifest.webmanifest',
-    '/js/index.js',
-    '/css/styles.css',
-    '/icons/icon-72x72.png',
-    '/icons/icon-96x96.png',
-    '/icons/icon-128x128.png',
-    '/icons/icon-144x144.png',
-    '/icons/icon-152x152.png',
-    '/icons/icon-192x192.png',
-    '/icons/icon-384x384.png',
-    '/icons/icon-512x512.png'
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/js/index.js',
+  '/css/styles.css',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
 ];
 
 
